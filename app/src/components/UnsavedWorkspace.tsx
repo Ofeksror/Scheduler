ff2bc6c5e27b4a9e9489a0deaf7c41ab2e78fb68
Ofek Sror <94007441+Ofeksror@users.noreply.github.com>
2023-06-24 20:38:41 +0300

    componments

---
+++ app/src/components/UnsavedWorkspace.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const UnsavedWorkspace = (props: Props) => {
+    return (
+        <div>
+            Hello
+        </div>
+    );
+};
+
+export default UnsavedWorkspace;
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.createElement("div");
+rootElement.id = "react-chrome-app";
+document.body.appendChild(rootElement);
+
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
