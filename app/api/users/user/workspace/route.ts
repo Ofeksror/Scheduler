0ceeb3eb1b7e87b423cea5e78da7b474d347e0e8
Ofek Sror <94007441+Ofeksror@users.noreply.github.com>
2023-07-21 20:23:58 +0300

    API endpoint: Attach workspace to User

---
+++ app/api/users/user/workspace/route.ts
@@ -0,0 +1,53 @@
+import dbConnect from "@/app/lib/dbConnect";
+import User from "@/app/models/User";
+import Workspace from "@/app/models/Workspace";
+import { ObjectId } from "mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+// ======================================================================= //
+
+/* ================================================ //
+// PUT: Attach to User (userId) a workspace (workspaceId)  
+// Parameters:
+//   1. JSON body: userId 
+//   2. JSON body: workspaceId 
+// Returns:
+//   - User's updated list of workspaces
+// ================================================ */
+export async function PUT(req: NextRequest) {
+    await dbConnect();
+
+    // Get parameters from request body
+    const { userId, workspaceId } = await req
+        .json()
+        .then((data: { userId: string; workspaceId: string }) => {
+            const userId = new ObjectId(data.userId);
+            const workspaceId = new ObjectId(data.workspaceId);
+            return { userId, workspaceId };
+        });
+
+    // Find user
+    const user = await User.findOne({ _id: userId });
+    if (!user)
+        return NextResponse.json(
+            { error: "Invalid User ID. User was not found in DB" },
+            { status: 400 }
+        );
+
+    // Check if workspace exists
+    if (await Workspace.findById(workspaceId) === null)
+        return NextResponse.json(
+            { error: "Invalid Workspace ID. Workspace was not found in DB" },
+            { status: 400 }
+        );
+
+    // Check if workspace already exists
+    if (user.workspaces.includes(workspaceId))
+        return NextResponse.json({message: "Workspace already associated with user"}, { status: 200 });
+
+    // Add new workspace
+    user.workspaces.push(workspaceId);
+    user.save();
+
+    return NextResponse.json({ workspaces: user.workspaces }, { status: 200 });
+}
