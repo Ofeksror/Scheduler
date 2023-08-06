a688c1aaaa899665b8ae6a2f39cd17754f26f194
Ofek Sror <94007441+Ofeksror@users.noreply.github.com>
2023-08-06 17:12:31 +0300

    UI component for deleting a workspace

--- components/DeleteWorkspace.tsx
+++ components/DeleteWorkspace.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
