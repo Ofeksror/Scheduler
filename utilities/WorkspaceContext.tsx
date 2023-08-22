3652087ee5e4d20bd3a10a2940359eb5ff24aa20
Ofek Sror <94007441+Ofeksror@users.noreply.github.com>
2023-08-22 16:25:51 +0300

    Save selectedWorkspace state when refreshing

--- utilities/WorkspaceContext.tsx
+++ utilities/WorkspaceContext.tsx
@@ -4,6 +4,7 @@ import { ObjectId } from "mongodb";
 import { createContext, useContext, useEffect, useRef, useState } from "react";
 import { useCookies } from "react-cookie";
 import { useDatabase } from "@/utilities/databaseContext";
+import mongoose from "mongoose";
 
 export type Workspace = {
     _id: ObjectId;
@@ -56,7 +57,7 @@ export const SelectedWorkspaceProvider: React.FC<ProviderProps> = ({
     children,
 }) => {
 
-    const { savedWorkspaces } = useDatabase();
+    const { savedWorkspaces, refreshWorkspace } = useDatabase();
 
     const [selectedWorkspace, setSelectedWorkspace] =
         useState<Workspace | null>(null);
@@ -65,6 +66,69 @@ export const SelectedWorkspaceProvider: React.FC<ProviderProps> = ({
     useEffect(() => {
         selectedWorkspaceRef.current = selectedWorkspace;
     }, [selectedWorkspace])
+
+    const hasRun = useRef(false);
+    const hasRun2 = useRef(false);
+
+    useEffect(() => {
+
+        if (!hasRun.current) {
+            hasRun.current = true;
+            return;
+        }
+
+        if (selectedWorkspace == null) {
+            localStorage.setItem("workspaceId", "");
+            return
+        }
+        localStorage.setItem("workspaceId", selectedWorkspace._id.toString());
+        localStorage.setItem("workspaceTitle", selectedWorkspace.title);
+        localStorage.setItem("workspaceTabs", JSON.stringify(selectedWorkspace.tabs));
+        localStorage.setItem("workspaceResources", JSON.stringify(selectedWorkspace.resources));
+    }, [selectedWorkspace])
+
+    const onReload = async () => {
+        const previousWorkspaceId = localStorage.getItem("workspaceId");
+        
+        if (previousWorkspaceId == "") {
+            setSelectedWorkspace(null);
+            return;
+        }
+
+        const _id: any = previousWorkspaceId || "";
+        const title = localStorage.getItem("workspaceTitle") || "";
+        const tabs = await JSON.parse(localStorage.getItem("workspaceTabs") || "");
+        const tabsUrls = tabs.map((tab: any) => tab.url);
+        const resources = await JSON.parse(localStorage.getItem("workspaceResources") || "");
+
+
+        setSelectedWorkspace({
+            _id,
+            title,
+            tabs,
+            tabsUrls,
+            resources,
+        })
+
+        refreshWorkspace({
+            _id,
+            title,
+            tabs,
+            tabsUrls,
+            resources,
+        })
+    }
+
+    useEffect(() => {
+        if (hasRun2.current) return;
+
+        if (savedWorkspaces !== null && savedWorkspaces.length !== 0) {
+            hasRun2.current = true;
+
+            onReload();
+        }
+        onReload();
+    }, [savedWorkspaces])
 
     const switchWorkspace = async (workspaceId: ObjectId) => {
         // Sync workspace before switching it
